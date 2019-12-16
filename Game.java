68477bd64dad193a688767ef29fc5fa3230e0484
hiba347 <58490871+hiba347@users.noreply.github.com>
2019-12-16 00:10:46 -0500

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,3 +1,17 @@
 public class Game {
-
+  //establish two players, one human one computer
+  //create player hands -- from player class
+  //create a loop that check when a player's and is empty -- this ends the game 
+      // while loops runs, human player asks computer player if they have a certain card, then computer player does the same 
+      // must compare hands, search the opposing player for the requested card 
+            // if found, give cards to the player that requested it 
+                  /*check for books -- go through the player's hand, int counter will increment each time the same rank(number
+                     is found, if counter reach 4, book counter for that player increases and those cards are removed from the 
+                     player's hand*/
+            // else, "go fish" 
+                // causes player that asked to draw a random card 
+  // if loop ends, compare books
+     //if human player's books counter are greater, they win and vice versa
+ //terminate program
+  
 }
