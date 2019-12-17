44a2e0c9fa3ef8cfe43d2b24555be3462426f5e8
hiba347 <58490871+hiba347@users.noreply.github.com>
2019-12-16 21:33:37 -0500

    Update Deck.java

--- Deck.java
+++ Deck.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Random;
 
 public class Deck {
@@ -7,12 +8,16 @@ public class Deck {
 
 	 public Deck(){ //initializes a deck of cards
 		 cards = new ArrayList<Card>();
-		 
-		 for(int i = 0; i <= 12; i++){
-			 cards.add(new Card(i));
+		 for (int j = 0; j < 4; j++) {		 
+			 for(int i = 0; i <= 12; i++){
+				 cards.add(new Card(i));
+			 }
 		 }
 	 }
 	 
+	 public void shuffle() {
+		 Collections.shuffle(cards);
+	 }
 	 public Card drawCard(){ //used when player needs to "Go Fish"
 		 Random generator = new Random();
 	     int index = generator.nextInt(cards.size());
@@ -20,8 +25,19 @@ public class Deck {
 	 }
 
 	 public String toString(){
-		 String result = "Cards left in deck: " + cards;
+		 String result = "Cards left in deck: " + cards.size();
 		 return result;
-
 	 }    
+	 public ArrayList<Card> getDeck() {
+		 return cards;
+	 }
+	 public void remove(int i) {
+		 cards.remove(i);
+	 }
+	 public void printDeck() {
+		 System.out.println("cards size is :" + cards.size());
+		 for (int i = 0; i < cards.size(); i++) {
+			 System.out.println(cards.get(i));
+		 }
+	 }
 }
